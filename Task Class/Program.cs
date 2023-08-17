0680a4fce147f960ea8b73fe1769f97e77421809
Egor <sorokin.egorka@gmail.com>
2023-08-17 14:15:48 +0300

    Добавьте файлы проекта.

---
+++ Task Class/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Task_Class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player max = new Player("Max", 100, 50, 23);
+
+            max.ShowDataPlayer();
+        }
+    }
+
+    class Player
+    {
+        public string Name;
+        public int Health;
+        public int Armor;
+        public int Damage;
+
+        public Player(string name, int health, int armor, int damage)
+        {
+            Name = name;
+            Health = health;
+            Armor = armor;
+            Damage = damage;
+        }
+
+        public void ShowDataPlayer()
+        {
+            Console.WriteLine($"Имя игрока: {Name}\nЗдоровье игрока: {Health}\nБроня игрока: {Armor}\nУрон Игрока: {Damage}");
+        }
+    }
+}
